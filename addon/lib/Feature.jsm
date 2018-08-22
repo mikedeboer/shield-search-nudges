mikedeboer/shield-search-nudges

commit 2ef3e09edca4eb1caa4711db3d3d7c6acecc31f7

Handle the user-initiated disable and uninstall triggers in the bootstrap handler methods. Fixes #15.

addon/lib/Feature.jsm
@@ -110,9 +110,6 @@ class Feature {
   async start(reason) {
     this.log.debug("Feature start");
 
-    // Listen for addon disabling or uninstall.
-    AddonManager.addAddonListener(this);
-
     // Perform something only during INSTALL = a new study period begins.
     if (reason === "ADDON_INSTALL") {
       this.resetPrefs();
@@ -153,6 +150,31 @@ class Feature {
     });
   }
 
+  /**
+   * Called at end of study, and if the user disables the study or it gets
+   * uninstalled by other means.
+   *
+   * @param {Boolean} [isUninstall]
+   */
+  async shutdown(isUninstall = false) {
+    if (isUninstall) {
+      this.resetPrefs(true);
+    }
+
+    this.stopTrackingForThisSession();
+
+    // If the panel was created in this window before, let's make sure to clean it up.
+    const winEnum = Services.wm.getEnumerator("navigator:browser");
+    while (winEnum.hasMoreElements()) {
+      const win = winEnum.getNext();
+      if (win.document && win.document.getElementById(TIP_PANEL_ID) && !win.closed) {
+        const {panel, panelButton} = this._ensurePanel(win);
+        panelButton.removeEventListener("command", this);
+        panel.remove();
+      }
+    }
+  }
+
   _addListenersForWindow(window) {
     window.gBrowser.addTabsProgressListener(this);
     window.gBrowser.tabContainer.addEventListener("TabSelect", this);
@@ -161,29 +183,6 @@ class Feature {
   _removeListenersFromWindow(window) {
     window.gBrowser.removeTabsProgressListener(this);
     window.gBrowser.tabContainer.removeEventListener("TabSelect", this);
-  }
-
-  /* START AddonListener interface methods. */
-  onUninstalling(addon) {
-    this.handleDisableOrUninstall(addon);
-  }
-
-  onDisabled(addon) {
-    this.handleDisableOrUninstall(addon);
-  }
-  /* END AddonListener interface methods. */
-
-  handleDisableOrUninstall(addon) {
-    if (addon.id !== this.studyUtils.config.addon.id) {
-      return;
-    }
-    // The in-scope AddonManager may already be garbage-collected at this point,
-    // so we need to import it again to be sure.
-    ChromeUtils.import("resource://gre/modules/AddonManager.jsm", {})
-      .AddonManager.removeAddonListener(this);
-    // This is needed even for onUninstalling, because it nukes the addon
-    // from UI. If we don't do this, the user has a chance to "undo".
-    addon.uninstall();
   }
 
   /**
@@ -272,32 +271,6 @@ class Feature {
           continue;
         }
         this._removeListenersFromWindow(win);
-      }
-    }
-  }
-
-  /**
-   * Called at end of study, and if the user disables the study or it gets
-   * uninstalled by other means.
-   *
-   * @param {Boolean} [isUninstall]
-   */
-  async shutdown(isUninstall = false) {
-    if (isUninstall) {
-      this.resetPrefs(true);
-    }
-
-    this.stopTrackingForThisSession();
-    AddonManager.removeAddonListener(this);
-
-    // If the panel was created in this window before, let's make sure to clean it up.
-    const winEnum = Services.wm.getEnumerator("navigator:browser");
-    while (winEnum.hasMoreElements()) {
-      const win = winEnum.getNext();
-      if (win.document && win.document.getElementById(TIP_PANEL_ID) && !win.closed) {
-        const {panel, panelButton} = this._ensurePanel(win);
-        panelButton.removeEventListener("command", this);
-        panel.remove();
       }
     }
   }

addon/bootstrap.js
@@ -134,10 +134,6 @@ this.Bootstrap = {
     const isUninstall =
       reason === studyUtils.REASONS.ADDON_UNINSTALL ||
       reason === studyUtils.REASONS.ADDON_DISABLE;
-    if (isUninstall) {
-      this.log.debug("uninstall or disable");
-    }
-
     if (isUninstall && !studyUtils._isEnding) {
       // we are the first 'uninstall' requestor => must be user action.
       this.log.debug("probably: user requested shutdown");
@@ -153,6 +149,17 @@ this.Bootstrap = {
     if (this.feature) {
       await this.feature.shutdown(isUninstall);
       delete this.feature;
+    }
+
+    if (isUninstall) {
+      this.log.debug("uninstall or disable");
+      // An in-scope AddonManager may already be garbage-collected at this point,
+      // so we need to import it here to be sure.
+      const addon = await ChromeUtils.import("resource://gre/modules/AddonManager.jsm", {})
+        .AddonManager.getAddonByID(addonData.id);
+      // This is needed even for onUninstalling, because it nukes the addon
+      // from UI. If we don't do this, the user has a chance to "undo".
+      addon.uninstall();
     }
 
     // Unload addon-specific modules

commit ba99703d9bf37f30b01e4d86624fc5441024bccb

Forgot to update this line in 2ef3e09edca4eb1caa4711db3d3d7c6acecc31f7, which caused study expiry to not work properly. Fixes #31.

addon/lib/Feature.jsm
@@ -193,7 +193,7 @@ class Feature {
     await this.studyUtils.endStudy({ reason: "expired" });
     const addon = await AddonManager.getAddonByID(this.studyUtils.config.addon.id);
     if (addon) {
-      this.handleDisableOrUninstall(addon);
+      addon.uninstall();
     }
   }
 